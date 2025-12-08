7944870012743ca92da7ff07388917dca1f87c43
lychen <happying910807@gmail.com>
2025-12-08 18:51:48 +0800

    IK rotation

--- Assets/MainAssets/Scripts/IKManager.cs
+++ Assets/MainAssets/Scripts/IKManager.cs
@@ -8,26 +8,42 @@ public class IKManager : MonoBehaviour
     public Transform rightHandTarget;
     public Transform leftHandTarget;
 
-    private float initialBarY;
-    private float initialLeftHandY;
-    private float initialRightHandY;
+    private Vector3 initialBarPosition;
+    private Vector3 initialLeftHandPosition;
+    private Vector3 initialRightHandPosition;
+    private Quaternion initialBarRotation;
+    private Quaternion initialLeftHandRotation;
+    private Quaternion initialRightHandRotation;
+
+    private Vector3 initialLeftHandOffset;
+    private Vector3 initialRightHandOffset;
+
+    private Vector3 minLeftHandTransform = new Vector3(-0.6f, 0.8f, 0.1f);
+    private Vector3 maxLeftHandTransform = new Vector3(-0.3f, 1.108f, 0.6f);
+    
+    private Vector3 minRightHandTransform = new Vector3(0.28f, 0.82f, 0.15f);
+    private Vector3 maxRightHandTransform = new Vector3(0.55f, 1.108f, 0.65f);
 
     // Start is called before the first frame update
     void Start()
     {
         if (bar != null)
         {
-            initialBarY = bar.position.y;
+            initialBarPosition = bar.position;
+            initialBarRotation = bar.rotation;
         }
         if (leftHandTarget != null)
         {
-            initialLeftHandY = leftHandTarget.position.y;
+            initialLeftHandPosition = leftHandTarget.position;
+            initialLeftHandRotation = leftHandTarget.rotation;
+            initialLeftHandOffset = initialLeftHandPosition - initialBarPosition;
         }
         if (rightHandTarget != null)
         {
-            initialRightHandY = rightHandTarget.position.y;
+            initialRightHandPosition = rightHandTarget.position;
+            initialRightHandRotation = rightHandTarget.rotation;
+            initialRightHandOffset = initialRightHandPosition - initialBarPosition;
         }
-        // Debug.LogWarning("[IKManager] initalBarY: " + initialBarY + " initialLeftHandY: " + initialLeftHandY + " initialRightHandY: " + initialRightHandY);
     }
 
     // Update is called once per frame
@@ -35,16 +51,63 @@ public class IKManager : MonoBehaviour
     {
         if (bar != null && leftHandTarget != null && rightHandTarget != null)
         {
-            float yDelta = bar.position.y - initialBarY;
+            // float yDelta = bar.position.y - initialBarPosition.y;
 
-            Vector3 newLeftPosition = leftHandTarget.position;
-            newLeftPosition.y = initialLeftHandY + yDelta;
+            // // left hand
+            // Vector3 newLeftPosition = leftHandTarget.position;
+            // newLeftPosition.y = initialLeftHandPosition.y + yDelta;
+            // if (newLeftPosition.y <= minLeftHandTransform.y)
+            // {
+            //     newLeftPosition.y = minLeftHandTransform.y;
+            // } 
+            // else if (newLeftPosition.y >= maxLeftHandTransform.y)
+            // {
+            //     newLeftPosition.y = newLeftPosition.y;
+            // }
+            // leftHandTarget.position = newLeftPosition;
+
+            // // right hand
+            // Vector3 newRightPosition = rightHandTarget.position;
+            // newRightPosition.y = initialRightHandPosition.y + yDelta;
+            // if (newRightPosition.y <= minRightHandTransform.y)
+            // {
+            //     newRightPosition.y = minRightHandTransform.y;
+            // }
+            // else if (newRightPosition.y >= maxRightHandTransform.y)
+            // {
+            //     newRightPosition.y = newRightPosition.y;
+            // }
+            // rightHandTarget.position = newRightPosition;
+
+            Quaternion rotationDelta = bar.rotation * Quaternion.Inverse(initialBarRotation);
+
+            // left hand
+            Vector3 rotatedLeftOffset = rotationDelta * initialLeftHandOffset;
+            Vector3 newLeftPosition = bar.position + rotatedLeftOffset;
+            if (newLeftPosition.y <= minLeftHandTransform.y)
+            {
+                newLeftPosition.y = minLeftHandTransform.y;
+            } 
+            else if (newLeftPosition.y >= maxLeftHandTransform.y)
+            {
+                newLeftPosition.y = newLeftPosition.y;
+            }
             leftHandTarget.position = newLeftPosition;
+            leftHandTarget.rotation = rotationDelta * initialLeftHandRotation;
 
-            Vector3 newRightPosition = rightHandTarget.position;
-            newRightPosition.y = initialRightHandY + yDelta;
+            // right hand
+            Vector3 rotatedRightOffset = rotationDelta * initialRightHandOffset;
+            Vector3 newRightPosition = bar.position + rotatedRightOffset;
+            if (newRightPosition.y <= minRightHandTransform.y)
+            {
+                newRightPosition.y = minRightHandTransform.y;
+            }
+            else if (newRightPosition.y >= maxRightHandTransform.y)
+            {
+                newRightPosition.y = newRightPosition.y;
+            }
             rightHandTarget.position = newRightPosition;
-
+            rightHandTarget.rotation = rotationDelta * initialRightHandRotation;
         }
     }
 }
